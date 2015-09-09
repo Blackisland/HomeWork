8352d08309d3f11a3f493014db5c6994b783c759
kittipas klinlek <kittipas.blackisland@gmail.com>
2015-09-09 23:29:42 +0700

    Up date Move Coroutine

---
+++ Assets/Scripts/Test_Coroutine.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test_Coroutine : MonoBehaviour {
+
+    public float smoothing = 1f;
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Move(target));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator Move(Transform target)
+    {
+        while(Vector3.Distance(transform.position,target.position)>0.05f)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, smoothing * Time.deltaTime);
+            yield return null;
+        }
+        print("Finish Move");
+        yield return new WaitForSeconds(3f);
+        print("Coroutine is Done.");
+    }
+}
